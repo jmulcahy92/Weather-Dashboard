cffe3c78279711c71863712a7dee0adc66858811
jmulcahy92 <jacobkmulcahy@gmail.com>
2023-02-07 00:03:47 -0600

    subdivided functionality; attempted fix of API urls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-// LINK TO WEATHER API
-
-// usable form
-    // user inputs a city and clicks search
-        // pull data on user's chosen city and populate current and future forecast elements in <article>
-            // default placeholder forecast? one from local storage or one of my own choice?
-        // create button in aside to recall/populate data for that same city again in the future
-            // buttons for every city in local storage present on page load
-        // store user's chosen city in local storage
-
 var APIKey = "d1af2c537e48864e4f4d6a0451359a75";
 var city;
 
@@ -16,15 +6,24 @@ var cityList = document.querySelector("#city-list");
 var weatherEl = document.querySelector("#current-weather");
 var forecastEl = document.querySelector("#five-day-forecast");
 
+// pull from local storage to populate aside buttons with previously searched cities
+
 function getWeather(event) {
     event.preventDefault();
     city = document.querySelector("#city-input").value;
-    var currentURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
-    var forecastURL  = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
+    document.querySelector("#city-input").value = "";
+    fetchData(city);
+}
+
+function fetchData(city) {
+    var currentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+    var forecastURL  = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
 
     fetch(currentURL)
         .then(function (response) {
             if (response.ok) {
+                // wrap button creation and local storage in if-statement to prevent repeating buttons/local storage
+
                 var newCityEl = document.createElement("button");
                 var newLiEl = document.createElement("li");
                 newCityEl.textContent = city;
@@ -41,7 +40,7 @@ function getWeather(event) {
                     var iconUrl = "http://openweathermap.org/img/wn/" + iconId + "@2x.png";
                     var iconEl = document.createElement("img");
                     iconEl.setAttribute("src", iconUrl);
-                    weatherEl.children[0].textContent = city + " " + "(" + date + ") "
+                    weatherEl.children[0].textContent = city + " " + "(" + date + ")"
                     weatherEl.children[0].appendChild(iconEl);
         
                     var temp = data.main.temp + " °F";
@@ -94,8 +93,9 @@ function getWeather(event) {
 }
 
 searchBtn.addEventListener("click", getWeather);
-
-// populate city buttons from local storage on page open
-// event listener for THESE buttons
-
-// prevent repeat entries in local storage/aside buttons?
+cityList.addEventListener("click", function (event) {
+    if(event.target.matches("button")) {
+        city = event.target.textContent;
+        fetchData(city);
+    }
+});
